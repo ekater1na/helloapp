ef0d70e942294d9039158a9e42679b49bbc9b159
ekater1na <ekaterina_l@tut.by>
2020-01-08 17:57:03 +0300

    feat: add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `<label>Введите имя:</label>
                  <input [(ngModel)]="name" placeholder="name">
-                 <h1>Добро пожаловать, {{name}}!</h1>`    
+                 <h1>Добро пожаловать, {{name}}!</h1>`,
+    styleUrls: ['./app.component.css'] 
 })
 export class AppComponent { 
     name= '';
